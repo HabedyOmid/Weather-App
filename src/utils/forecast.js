61185b009c5136ea0010fcf510ac2a14157a040a
Fluffy <fluffy@Fluffys-iMac.local>
2020-03-28 09:42:38 -0500

    async added

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -2,24 +2,28 @@ const axios = require('axios');
 
 // weather forecast by providing address
 const forecast = (lat, lng, callback) => {
-  axios
-    .get(
-      `https://api.darksky.net/forecast/9c693d80dd58e2cc4d5f160ecb9ef15e/${lat},${lng}?units=ca`
-    )
 
-    .then(response => {
-      callback(undefined, response.data);
-    })
+  setTimeout(() =>{
+    axios
+      .get(
+        `https://api.darksky.net/forecast/9c693d80dd58e2cc4d5f160ecb9ef15e/${lat},${lng}?units=ca`
+      )
 
-    .catch(err => {
-      if (err.response) {
-        callback('Something went wrong with server. Try again later!');
-      } else if (err.request) {
-        callback('Unable to find location, Try another address!');
-      } else {
-        callback('Unable to find location, Try another address!');
-      }
-    });
+      .then(response => {
+        callback(undefined, response.data);
+      })
+
+      .catch(err => {
+        if (err.response) {
+          callback('Something went wrong with server. Try again later!');
+        } else if (err.request) {
+          callback('Unable to find location, Try another address!');
+        } else {
+          callback('Unable to find location, Try another address!');
+        }
+      });
+
+    }, 500);
 };
 
 module.exports = forecast;
